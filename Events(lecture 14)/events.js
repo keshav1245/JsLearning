6d82a1d6f042ea0f05dea368396aa2a15b0a04b8
Keshav Tangri <tangri57@gmail.com>
2019-03-30 16:15:45 +0530

    Add files via upload

--- Events(lecture 14)/events.js
+++ Events(lecture 14)/events.js
@@ -44,3 +44,41 @@ btn2.addEventListener('mouseover',function(){
 btn2.addEventListener('mouseout',function(){
 	btn2.style.backgroundColor = 'white';
 });
+
+
+
+////////////////////event targets/////////////////////////
+// Here you can see that we are including an event object, e, in the function, and in the function setting a 
+// background color style on e.target — which is the button itself. The target property of the event object 
+// is always a reference to the element that the event has just occurred upon. So in this example, we are setting 
+// a random background color on the button, not the page.
+var eventbtn = document.querySelector('.eventbtn');
+
+eventbtn.addEventListener('click',bgChange);
+function bgChange(e) {
+  var rndCol = 'rgb(' + randomn(255) + ',' + randomn(255) + ',' + randomn(255) + ')';
+  e.target.style.backgroundColor = rndCol;
+  console.log(e);
+}  
+
+
+
+
+
+
+//PREVENT DEFAULT - when you want to prevent some activity because of wrong info or task performed .. example
+//submitting form with info filled.
+
+var form = document.querySelector('form');
+var fname = document.getElementById('fname');
+var lname = document.getElementById('lname');
+var submit = document.getElementById('submit');
+var para = document.querySelector('p');
+
+form.onsubmit = function(e) {
+  if (fname.value === '' || lname.value === '') {
+    e.preventDefault();
+    para.textContent = 'You need to fill in both names!';
+  }
+}
+
