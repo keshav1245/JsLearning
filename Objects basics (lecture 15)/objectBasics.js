f9530b25f626d610efc7c4e7da12d703104510d0
Keshav Tangri <tangri57@gmail.com>
2019-04-02 20:43:04 +0530

    Add files via upload

--- Objects basics (lecture 15)/objectBasics.js
+++ Objects basics (lecture 15)/objectBasics.js
@@ -44,7 +44,52 @@ person = {
 		alert('Hi ! I\'m '+this.name[0]+'.');
 	}
 };
-
-
+//dot notation
 console.log(person.name.first);
 console.log(person.name.last);
+//brackett notation
+console.log(person['name']['first']);
+console.log(person['name']['last']);
+
+//Bracket notation
+console.log('Age by brackett notation : '+person['age']);
+//dot Notation
+console.log('Age by dot notation : '+person.age);
+
+//changing valus 
+console.log("Last name is : "+person.name.last);
+person.name.last = 'McClause';
+console.log('Now Last name is : '+person.name.last);
+
+console.log("Creating new members in the object : ");
+console.log("Eye color : "+person.eyesColor+" \nHeight : "+person.height);
+person.eyesColor = 'Brown';
+person.height = 1.12;
+console.log("Eye color : "+person.eyesColor+" \nHeight : "+person.height);
+person.farewell = function(){
+	alert("Will meet again soon !");
+}
+
+//Saving user input into our object 
+var food = 'Food';
+var favfood = prompt("Which Food Item you like the Most ?");
+person[food] = favfood;
+
+console.log(person.Food);
+
+//this keyword - The this keyword refers to the current object the code is being written inside
+var fruit1 = {
+	type : 'seedless',
+	display : function(){
+		console.log("This fruit is : "+this.type);
+		// console.log("This fruit is : "+type);
+	}	
+}
+var fruit2 = {
+	type : 'with seed',
+	display : function(){
+		console.log("This fruit is : "+this.type);
+	}	
+}
+console.log(fruit1.display());
+console.log(fruit2.display());
