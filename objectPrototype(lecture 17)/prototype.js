984ac6f76129d994b52cf5ad69c139541f73aee0
Keshav Tangri <tangri57@gmail.com>
2019-04-04 20:29:02 +0530

    Add files via upload

---
+++ objectPrototype(lecture 17)/prototype.js
@@ -0,0 +1,42 @@
+// // prototype-based language — to provide inheritance, objects can have a prototype object, which acts as a 
+// // template object that it inherits methods and properties from.
+// An object's prototype object may also have a prototype object, which it inherits methods and properties from, and so on. This is often referred to as a prototype chain, and explains why different objects have properties and methods defined on other objects available to them.
+
+// Well, to be exact, the properties and methods are defined on the prototype property on the Objects' 
+//constructor functions, not the object instances themselves
+
+function Person(first, last, age, gender, interests){
+	this.name = {
+		'first' : first,
+		'last' : last
+	};
+	this.age = age;
+	this.gender = gender;
+	this.interests = interests;
+	this.bio = function(){
+		alert(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+
+	};
+	this.greeting = function(){
+		alert('Hi ! I\'m '+this.name.first+'.');
+	};
+}
+
+var person1 =  new Person('keshav','tangri',20,'Male',['music','skiing']);
+console.log(person1.__proto__);
+console.log(person1.__proto__.__proto__);
+console.log(Person.prototype);
+console.log(Object.prototype);
+
+var myString = "This is a string";
+console.log(myString.__proto__);//prototype functions of String
+var myArray = ['element1','element2'];
+console.log(myArray.__proto__);//prototype functions of array
+var myNumber = 42;
+console.log(myNumber.__proto__);
+
+var person2 = Object.create(person1);
+console.log(person2.__proto__);
+
+
+
