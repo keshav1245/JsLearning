7486587296ed5f1632d614fe860b7eec171e9dd8
Keshav Tangri <tangri57@gmail.com>
2019-04-05 05:59:33 +0530

    Add files via upload

--- objectPrototype(lecture 17)/prototype.js
+++ objectPrototype(lecture 17)/prototype.js
@@ -38,5 +38,32 @@ console.log(myNumber.__proto__);
 var person2 = Object.create(person1);
 console.log(person2.__proto__);
 
+console.log(person1.constructor);
+console.log(person2.constructor);
 
+// A clever trick is that you can put parentheses onto the end of the constructor property (containing any 
+// required parameters) to create another object instance from that constructor. The constructor is a function 
+// after all, so can be invoked using parentheses; you just need to include the new keyword to specify that you 
+// want to use the function as a constructor.
+
+var person3 = new person1.constructor('Shreya','Tangri',25,'female',['eating','chanting']);
+console.log(person3.name);
+console.log(person3.bio());
+console.log(person3.constructor);
+console.log(person3.constructor.name);
+console.log(person1.constructor.name);
+console.log(person3.__proto__);
+
+
+Person.prototype.farewell = function(){
+	alert(this.name.first + " is bidding us farewell");
+};
+
+person1.farewell();
+person2.farewell();
+person3.farewell();
+
+Person.prototype.fullName = this.name.first + ' ' + this.name.last;
+
+console.log(person3.fullName);//does not work
 
