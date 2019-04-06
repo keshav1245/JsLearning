32643852d8d419410c63f5d6d04db64c6e5e096b
Keshav Tangri <tangri57@gmail.com>
2019-04-06 23:34:34 +0530

    Add files via upload

--- InheritanceJs(lecture 18)/inherit.js
+++ InheritanceJs(lecture 18)/inherit.js
@@ -120,3 +120,106 @@ teacher1.bio();
 teacher1.subject;
 teacher1.greeting();
 teacher1.farewell();
+
+
+//making the student cconstructor inherit the person 
+
+function Student(first,last,age,gender,interests){
+  Person.call(this,first,last,age,gender,interests);
+}
+Student.prototype = Object.create(Person.prototype);
+
+Object.defineProperty(Student.prototype,'constructor',{
+  value : Student,
+  enumerable : false,
+  writable : true
+});
+
+Student.prototype.greeting = function(){
+  alert("Yo! I am "+this.name.first);
+}
+
+var student1 = new Student('Tammi', 'Smith', 17, 'female', ['music', 'skiing', 'kickboxing']);
+
+student1.greeting();
+
+
+//ECMA Script 2015 Classes
+
+class Person2 {
+  constructor(first,last,age,gender,interests){
+    this.name = {
+      first,last
+    };
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests
+  }
+
+  greeting(){
+    console.log(`Hi! I\'m ${this.name.first}`);
+  };
+
+  farewell() {
+    console.log(`${this.name.first} has left the building. Bye for now !`);
+  };
+
+}
+
+// The class statement indicates that we are creating a new class. Inside this block, we define all the 
+// features of the class:
+
+// The constructor() method defines the constructor function that represents our Person class.
+// greeting() and farewell() are class methods. Any methods you want associated with the class are defined 
+// inside it, after the constructor. In this example, we've used template literals rather than string 
+// concatenation to make the code easier to read.
+
+let tangri = new Person2('keshav','tangri',20,'Male',['Coding','Photography']);
+tangri.greeting();
+tangri.farewell();
+
+class Teacher2 extends Person2{
+  constructor(first,last,age,gender,interests,subject,grade){
+    super(first,last,age,gender,interests);                        //calling the parent constructor
+    this.subject = subject;
+    this.grade = grade;
+  }
+
+  bio(){
+    console.log(`Prof. ${this.name.last} is ${this.age} years old and teaches ${this.subject}`);
+  };
+
+  get subject(){
+    return this._subject;
+  }
+
+  set subject(subjectval){
+    this._subject = subjectval;
+  }
+
+}
+
+
+let tedMosby = new Teacher2('Ted','Mosby',40,'Male',['Architecture facts','Mosby boys detective'],'Architecture 101','High School');
+
+tedMosby.greeting();
+tedMosby.farewell();
+tedMosby.bio();
+
+
+
+//getter setters
+// In our class above we have a getter and setter for the subject property. We use _  to create a separate 
+// value in which to store our name property. Without using this convention, we would get errors every time 
+// we called get or set. At this point:
+
+// To show the current value of the _subject property of the snape object we can use the snape.subject getter 
+// method. To assign a new value to the _subject property we can use the snape.subject="new value" setter method.
+
+console.log(tedMosby.subject);
+tedMosby.subject = "Eco 201";
+console.log(tedMosby.subject);
+
+
+
+
